6536f85cf40a4e37957a959ccf82eb6eed9486a3
MatthewWertman <mattwert@charter.net>
2021-06-28 12:54:54 -0700

    further clean up + add comments

--- src/main/java/com/budaslounge/gui/gui.java
+++ src/main/java/com/budaslounge/gui/gui.java
@@ -61,44 +61,59 @@ public class gui {
 
     public static void addComponentsToPane(Container pane) {
 
-        // Setting up the content pane
-        pane.setLayout(new GridLayout());
+        // Set up content pane
+        pane.setLayout(new GridLayout());       // Set layout to GridLayout
         pane.setMinimumSize(new Dimension(500,500));
         pane.setPreferredSize(new Dimension(850,500));
+        // Add listener on component resize
         pane.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent componentEvent) {
+                // Casting magic
                 JPanel f = (JPanel) componentEvent.getComponent();
                 JSplitPane splitPane = (JSplitPane) f.getComponents()[0];
-                Dimension frameSize = componentEvent.getComponent().getBounds().getSize();
-                splitPane.setDividerLocation(frameSize.width / 3);
+                Dimension frameSize = componentEvent.getComponent().getBounds().getSize();      // Get the current frame size
+                splitPane.setDividerLocation(frameSize.width / 3);      // Set SplitPane divider to 1/3 of frame size
             }
         });
 
+        // Create SplitPane
         splitPane = new JSplitPane();
         splitPane.setDividerSize(5);
         pane.add(splitPane);
 
+        // Create Console Pane
         textPane = new JTextPane();
         textPane.setEditable(false);
         textPane.setText("Console output goes here..");
         splitPane.setRightComponent(textPane);
 
+        /*
+        *
+        Below we create what is called the "ActionPanel". This houses all of the selections and options on the left
+        side of the Split Pane.
+        *
+        The ActionPanel uses the GridBagLayout layout manager. It is the more sophisticated cousin of the GridLayout.
+        This allows components to span over multiple rows and/or columns while keeping a grid-like layout.
+        *
+        With the components defined below, this creates a 15 row, 2 column grid.
+        */
+        // Create ActionPanel
         panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
         splitPane.setLeftComponent(panel);
-        GridBagConstraints gbc = new GridBagConstraints();
+        panel.setLayout(new GridBagLayout());   // Set layout to GridBagLayout
+        GridBagConstraints gbc = new GridBagConstraints();      // Initialize GridBagConstraints
 
-        // Top label
+
+        // Create top label
         label = new JLabel();
         label.setText("Select Game Version");
         label.setFont(headerFont);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.anchor = GridBagConstraints.PAGE_START;
-        gbc.gridwidth = 2;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets = new Insets(5, 0,0,0);
+        gbc.gridwidth = 2;  // take up both columns
+        gbc.insets = new Insets(5, 0, 0, 0);    // set top padding to 5
+        gbc.fill = GridBagConstraints.CENTER;       // Center text
         panel.add(label, gbc);
 
         // Game version combo box
@@ -109,148 +124,148 @@ public class gui {
         cmbGameVersionModel.addElement("GameCube");
         cmbGameVersionModel.addElement("PlayStation");
         cmbGameVersion.setModel(cmbGameVersionModel);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 2;
         gbc.gridx = 0;
-        gbc.gridy = 1;
+        gbc.gridy = 1;  // new row
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(cmbGameVersion, gbc);
 
         // "Tools" label
         label = new JLabel();
         label.setText("Tools");
         label.setFont(headerFont);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 2;
         gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.insets = new Insets(20, 0, 10, 0);
+        gbc.gridy = 2;  // new row
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(20, 0, 10, 0);      // top padding: 20, bottom padding: 10
         panel.add(label, gbc);
 
         // "ISOs" label
         label = new JLabel();
         label.setText("    ISOs");
         label.setFont(subHeaderFont);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = 2;
         gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.insets = new Insets(5,0,5,0);
+        gbc.gridy = 3;  // new row
+        gbc.gridwidth = 2;
+        gbc.insets = new Insets(5,0,5,0);       // top padding: 5, bottom padding: 5
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(label, gbc);
 
         rbNgcisoTool = new JRadioButton("Ngciso");
         isoSelectGrp.add(rbNgcisoTool);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 1;
-        gbc.weightx = 0.5;
         gbc.gridx = 0;
         gbc.gridy = 4;
-        gbc.insets = new Insets(0,0,0,0);
+        gbc.gridwidth = 1;  // only take up one column
+        gbc.weightx = 0.5;  // take extra column space
+        gbc.insets = new Insets(0,0,0,0);       // reset padding
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(rbNgcisoTool, gbc);
 
         rbPsisoTool = new JRadioButton("Psiso");
         isoSelectGrp.add(rbPsisoTool);
-        gbc.fill = GridBagConstraints.CENTER;
+        gbc.gridx = 1;  // place on second column
+        gbc.gridy = 4;
         gbc.gridwidth = 1;
         gbc.weightx = 0.5;
-        gbc.gridx = 1;
-        gbc.gridy = 4;
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(rbPsisoTool, gbc);
 
         // "Archives" label
         label = new JLabel();
         label.setText("    Archives");
         label.setFont(subHeaderFont);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = 2;
         gbc.gridx = 0;
         gbc.gridy = 5;
+        gbc.gridwidth = 2;
         gbc.insets = new Insets(5, 0, 5, 0);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(label, gbc);
 
         rbBecTool = new JRadioButton("Bec Tool");
         archiveSelectGrp.add(rbBecTool);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 1;
-        gbc.weightx = 0.5;
         gbc.gridx = 0;
         gbc.gridy = 6;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.5;
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(rbBecTool, gbc);
 
         rbPakTool = new JRadioButton("Pak Tool");
         archiveSelectGrp.add(rbPakTool);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 1;
-        gbc.weightx = 0.5;
         gbc.gridx = 1;
         gbc.gridy = 6;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.5;
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(rbPakTool, gbc);
 
         rbTokTool = new JRadioButton("Tok Tool");
         archiveSelectGrp.add(rbTokTool);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 1;
-        gbc.weightx = 0.5;
         gbc.gridx = 0;
         gbc.gridy = 7;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.5;
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(rbTokTool, gbc);
 
         rbZlibTool = new JRadioButton("Zlib Tool");
         archiveSelectGrp.add(rbZlibTool);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 1;
-        gbc.weightx = 0.5;
         gbc.gridx = 1;
         gbc.gridy = 7;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.5;
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(rbZlibTool, gbc);
 
         // "Mode" label
         label = new JLabel();
         label.setText("Select Mode");
         label.setFont(headerFont);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 2;
         gbc.gridx = 0;
         gbc.gridy = 8;
+        gbc.gridwidth = 2;
         gbc.insets = new Insets(20, 0, 10, 0);
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(label, gbc);
 
         rbModeUnpack = new JRadioButton("Unpack");
         modeSelectGrp.add(rbModeUnpack);
         rbModeUnpack.setSelected(true);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 1;
-        gbc.weightx = 0.5;
         gbc.gridx = 0;
         gbc.gridy = 9;
-        gbc.insets = new Insets(0,0,0,0);   //reset
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.5;
+        gbc.insets = new Insets(0,0,0,0);   // reset padding
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(rbModeUnpack, gbc);
 
         rbModePack = new JRadioButton("Pack");
         modeSelectGrp.add(rbModePack);
-        gbc.fill = GridBagConstraints.CENTER;
-        gbc.gridwidth = 1;
-        gbc.weightx = 0.5;
         gbc.gridx = 1;
         gbc.gridy = 9;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.5;
+        gbc.fill = GridBagConstraints.CENTER;
         panel.add(rbModePack, gbc);
 
         txtSourceLocation = new JTextField();
         txtSourceLocation.setEditable(false);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = 2;
-        gbc.weightx = 1.0;
         gbc.gridx = 0;
         gbc.gridy = 10;
+        gbc.gridwidth = 2;
+        gbc.weightx = 1.0;
         gbc.insets = new Insets(20, 0, 0, 0);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(txtSourceLocation, gbc);
 
         btnSourceLoc = new JButton("Find file to be extracted");
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = 2;
-        gbc.weightx = 1.0;
         gbc.gridx = 0;
         gbc.gridy = 11;
+        gbc.gridwidth = 2;
+        gbc.weightx = 1.0;
         gbc.insets = new Insets(5, 0,0,0);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(btnSourceLoc, gbc);
 
         btnSourceLoc.addActionListener(new ActionListener() {
@@ -273,20 +288,19 @@ public class gui {
 
         txtOutputLocation = new JTextField();
         txtOutputLocation.setEditable(false);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = 2;
-        gbc.weightx = 1.0;
         gbc.gridx = 0;
         gbc.gridy = 12;
-        //gbc.insets = new Insets(10, 20, 0 , 20);
+        gbc.gridwidth = 2;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(txtOutputLocation, gbc);
 
         btnOutputLoc = new JButton("Choose output directory");
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = 2;
-        gbc.weightx = 1.0;
         gbc.gridx = 0;
         gbc.gridy = 13;
+        gbc.gridwidth = 2;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(btnOutputLoc, gbc);
 
         btnOutputLoc.addActionListener(new ActionListener() {
@@ -303,15 +317,15 @@ public class gui {
         // Bottom button
         btnRunTool = new JButton("Run tool");
         btnRunTool.setFont(subHeaderFont);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-        gbc.ipady = 20;
-        gbc.insets = new Insets(0,0,0,0);
-        gbc.anchor = GridBagConstraints.LAST_LINE_START;
         gbc.gridx = 0;
-        gbc.gridwidth = 2;
         gbc.gridy = 14;
+        gbc.gridwidth = 2;
+        gbc.weightx = 1.0;  // take any extra column space
+        gbc.weighty = 1.0;  // take any extra row space
+        gbc.ipady = 20;     // give some extra height
+        gbc.insets = new Insets(0,0,0,0);   // reset padding
+        gbc.anchor = GridBagConstraints.LAST_LINE_START;    // anchor component to bottom left corner
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(btnRunTool, gbc);
     }
 
@@ -344,11 +358,6 @@ public class gui {
             java.util.logging.Logger.getLogger(gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
 
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+        javax.swing.SwingUtilities.invokeLater(gui::createAndShowGUI);
     }
 }
